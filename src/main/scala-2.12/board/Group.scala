4fc38e61b4231dc881c725ee2faaf2b9c5191fa4
Dryden Bouamalay <bouamalayd@gmail.com>
2017-07-04 00:15:15 -0500

    removed board package

--- src/main/scala-2.12/board/Group.scala
+++
@@ -1,7 +0,0 @@
-//package board
-//
-///**
-//  * Created by Dryden Bouamalay on 3/21/17.
-//  */
-//class Group(stones: Set[Stone]) {
-//}
--- src/main/scala-2.12/board/package.scala
+++
@@ -1,7 +0,0 @@
-/**
-  * Created by Dryden Bouamalay on 3/25/17.
-  */
-package object board {
-  val MIN_BOARD_COORD = 0
-  val MAX_BOARD_COORD = 19
-}
